dc2f67bcd97c7da779c3a073dd4d27d61b5f2385
Matt Costello <mjgcostello@gmail.com>
2015-06-22 17:17:13 +0100

    Data tidied

--- Data/scripts/LTI rates graphs.R
+++ Data/scripts/LTI rates graphs.R
@@ -1,8 +1,16 @@
 require(ggplot2)
 require(zoo)
 require(reshape2)
+require(RColorBrewer)
+require(tidyr)
+require(dplyr)
 
 setwd("~/Academic/SGPE/Dissertation/Data/csv/")
 
 data <- read.csv("long_term_interest_rates_ECB.csv")
 data$date <- as.yearmon(data$date, "%Y%b")
+
+x <- colnames(data)
+
+df <- data %>% 
+  gather(country, yield, Austria..Euro:Slovakia..Euro)
